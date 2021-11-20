51b6a33e1a5d4be2be428aef976387021837105b
michael.yeo <michael.yeo@temus.com>
2021-11-20 14:30:01 +0800

    Added new read DTO

---
+++ Dtos/Users/UserReadDto.cs
@@ -0,0 +1,9 @@
+namespace Store.Dtos.Users
+{
+    public class UserReadDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+    }
+}
